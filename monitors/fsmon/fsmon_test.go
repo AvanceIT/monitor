2db38ccd6182600b04dff05126fc5374428b1024
Jonathan Pulfer <JonPulfer@server.fake>
2013-09-13 20:43:54 +0100

    Basic test set up

--- monitors/fsmon/fsmon_test.go
+++ monitors/fsmon/fsmon_test.go
@@ -7,3 +7,11 @@ import (
 func TestGetFsInfo(t *testing.T) {
 	_ = getFsInfo("/")
 }
+
+func TestConfigMonitor(t *testing.T) {
+	fs := configMonitor()
+	fsLen := len(fs.FsConfigs)
+	if fsLen == 0 {
+		t.Fail()
+	}
+}
