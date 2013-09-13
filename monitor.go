094d976cb68823688afdaf5ab4186c73604e0e2c
Jonathan Pulfer <JonPulfer@server.fake>
2013-09-13 08:48:37 +0100

    Removed BUG comment for config file problem

--- monitor.go
+++ monitor.go
@@ -1,8 +1,6 @@
 // Monitor provides a client side monitoring suite
 package main
 
-//BUG(JP) Needs an exception handler if the config files are not present :) 
-
 import (
 	"fmt"
 	"github.com/AvanceIT/monitor/monitors/procmon"
